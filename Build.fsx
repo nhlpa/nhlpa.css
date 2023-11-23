dead797bf0c04af6f766c5605efe078913006d1b
Pim Brouwers <pbrouwers@nhlpa.com>
2023-11-23 06:57:40 -0500

    color atoms

--- Build.fsx
+++ Build.fsx
@@ -1,12 +1,6 @@
-open System
 open System.IO
 open System.Text
 open System.Text.RegularExpressions
-
-let sb = StringBuilder()
-let append (x : string) = sb.AppendLine(x) |> ignore
-let appendLine () = sb.AppendLine() |> ignore
-
 
 let breakpoints =
     [
@@ -22,7 +16,13 @@ let cssFiles =
         "nhlpa.atoms.sizing.css"
         "nhlpa.atoms.spacing.css"
         "nhlpa.atoms.text.css"
+        "nhlpa.atoms.color.css"
     ]
+
+let sb = StringBuilder()
+
+let append (x : string) = sb.AppendLine(x) |> ignore
+let appendLine () = sb.AppendLine() |> ignore
 
 cssFiles
 |> List.map (fun cssPath ->
@@ -34,12 +34,16 @@ cssFiles
 |> List.map snd
 |> String.concat "\n\n"
 |> fun breakpointCss ->
-    let breakpointCssAtoms breakpointSuffix =
-        Regex.Replace(breakpointCss, @"^(\.[\w\-\d]+)", $"  $1{breakpointSuffix}", RegexOptions.Multiline)
-
     for (suffix, width) in breakpoints do
+        let breakpointCssAtoms =
+            Regex.Replace(
+                input = breakpointCss,
+                pattern = @"^(\.[\w\-\d]+)",
+                replacement = $"  $1{suffix}",
+                options = RegexOptions.Multiline)
+
         append $"@media screen and (min-width: {width}) {{"
-        append (breakpointCssAtoms suffix)
+        append breakpointCssAtoms
         append "}"
 
 File.WriteAllText (Path.Join(__SOURCE_DIRECTORY__, "nhlpa.css"), sb.ToString())
